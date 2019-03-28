1dc8546bac004cedf5a9ce59aa5e3c4d7fe16fd4
alvalle09 <phredvalle@gmail.com>
2019-03-27 21:45:23 -0500

    comments to aid in grasping state, store, dispatch, subscribe

--- app.js
+++ app.js
@@ -2,34 +2,39 @@
  on Pluralsight. Two thumbs up!  👍👍
 */
 
-// 
+// import redux
 const { createStore } = require('redux');
 
+// initialize state
 const defaultState = {
   courses: [
     {
       name: 'Learning React',
-      topic: 'React',
+      topic: 'React'
     },
     {
       name: 'Learning Angular',
-      topic: 'Angular',
+      topic: 'Angular'
     },
     {
       name: 'Using Redux with Angular',
-      topic: 'Angular',
+      topic: 'Angular'
     }
   ]
 };
 
+
+// Reducer calculates the updated state 
 function reducer(state, action) {
+
   switch (action.type) {
     case 'ADD_COURSE':
       return Object.assign({}, state, {
-        courses: [...state.courses, action.course]
+        courses: [...state.courses, action.course]   // use spread operator ... to copy 
       });
+
     default:
-      return state;
+      return state;  // when action is unknown, return current state
   }
 }
 
@@ -37,6 +42,7 @@ const store = createStore(reducer, defaultState);
 
 function addView(viewFunc) {
   viewFunc(defaultState);
+  // subscribe to change events
   store.subscribe(()=> {
     viewFunc(store.getState());
   });
@@ -50,14 +56,13 @@ addView((state) => {
   console.log(`The latest course in the library: ${state.courses[state.courses.length -1].name}`);
 });
 
-
+// the only way to change state is to dispatch
 store.dispatch({
   type: 'ADD_COURSE',
   course: {
-    name: "A new course in Redux!",
-    topic: 'New course on Redux & Angular'
+    name: 'A new course in Redux!',
+    topic: 'Redux'
   }
-
 });
 
 
